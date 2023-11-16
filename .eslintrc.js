d87b67e3549173719e4fe3430817fa72ffdb046f
Anmol Kumar <anmolshrivastav.08@gmail.com>
2023-11-16 16:26:40 +0530

    Disabled no missing localization rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,2 +1,12 @@
-// eslint-disable-next-line import/extensions
-module.exports = require("@bigbinary/neeto-commons-frontend/configs/eslint/index.js");
+const defaultConfig = require("@bigbinary/neeto-commons-frontend/configs/eslint/index.js");
+
+const { mergeDeepLeft } = require("ramda");
+
+module.exports = mergeDeepLeft(
+  {
+    rules: {
+      "@bigbinary/neeto/no-missing-localization": "off",
+    },
+  },
+  defaultConfig
+);
