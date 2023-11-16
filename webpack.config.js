13bbc13fef40e489509300c9fc09155959021e54
Anmol Kumar <anmolshrivastav.08@gmail.com>
2023-11-16 17:01:47 +0530

    Added webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path'); 
+const path = require("path");
 
 module.exports = {
-  entry: './dist/index.js',
+  entry: "./dist/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
   },
-  mode: 'production',
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
           },
         },
       },
