6447895c8f5d5935879c82e63448f61d412ea021
Anmol Kumar <anmolshrivastav.08@gmail.com>
2023-11-16 17:05:46 +0530

    Added some missing configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./dist/index.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
