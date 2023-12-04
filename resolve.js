238f387da9bf519030cb8d0e3aed750ddba70a78
Deepak Jose <deepakjosp@gmail.com>
2023-12-04 15:58:12 +0530

    Added aliases for constants and utils and fixed issues in rollup.config

---
+++ resolve.js
@@ -0,0 +1,8 @@
+const path = require("path");
+
+module.exports = {
+  alias: {
+    constants: path.resolve(__dirname, "./src/constants"),
+    utils: path.resolve(__dirname, "./src/utils"),
+  },
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,6 @@
+import path from "path";
+
+import alias from "@rollup/plugin-alias";
 import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
@@ -5,39 +8,48 @@ import replace from "@rollup/plugin-replace";
 import analyze from "rollup-plugin-analyzer";
 import cleaner from "rollup-plugin-cleaner";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import { mergeDeepLeft } from "ramda";
+
+import packageJson from "./package.json";
+
+const commonResolve = require("@bigbinary/neeto-commons-frontend/configs/nanos/webpack/resolve.js");
+const projectResolve = require("./resolve.js");
+
+const formats = ["esm", "cjs"];
+const { alias: aliasEntries } = mergeDeepLeft(projectResolve, commonResolve);
+const peerDependencies = Object.keys(packageJson.peerDependencies);
 
 const plugins = [
-  cleaner({
-    targets: ["./dist/"],
-  }),
+  cleaner({ targets: [path.resolve(__dirname, "dist")] }),
+  // To automatically externalize peerDependencies in a rollup bundle.
   peerDepsExternal(),
   replace({
     preventAssignment: true,
   }),
   resolve({
-    browser: true,
     preferBuiltins: true,
     extensions: [".js", ".jsx", ".svg"],
   }),
-  commonjs(),
-  babel({ babelHelpers: "runtime" }),
+  commonjs({ sourceMap: true }),
+  alias({ entries: aliasEntries }),
+  babel({
+    babelHelpers: "runtime",
+    sourceMaps: true,
+  }),
   analyze({ summaryOnly: true }),
 ];
 
-const formats = ["esm", "cjs"];
+const output = formats.map(format => ({
+  assetFileNames: "[name][extname]",
+  file: format === "esm" ? "dist/index.js" : "dist/index.cjs.js",
+  format,
+  name: "NeetoHotkeysNano",
+  sourcemap: true,
+}));
 
-const getOutputConfigs = outputPath =>
-  formats.map(format => ({
-    file: format === "esm" ? `${outputPath}.js` : `${outputPath}.cjs.js`,
-    format,
-    sourcemap: true,
-    inlineDynamicImports: true,
-  }));
-
-export default [
-  {
-    input: "src/index.js",
-    output: getOutputConfigs("dist/index"),
-    plugins,
-  },
-];
+export default {
+  input: "./src/index.js",
+  output,
+  external: peerDependencies,
+  plugins,
+};
