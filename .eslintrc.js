0e991e35e26968a899d75a59c518ced369adedc3
Deepak Jose <deepakjosp@gmail.com>
2023-12-04 19:10:52 +0530

    #3 - Set `@bigbinary/neeto/webpack-aliases-and-jsconfig-paths-should-be-in-sync` to off since we don't have jsconfig.json file in the path specified in the rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = mergeDeepLeft(
   {
     rules: {
       "@bigbinary/neeto/no-missing-localization": "off",
+      "@bigbinary/neeto/webpack-aliases-and-jsconfig-paths-should-be-in-sync":
+        "off",
     },
   },
   defaultConfig
