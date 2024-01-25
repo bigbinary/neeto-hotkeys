c4046dbe665711276417e7fc7102de59cd28d2bd
Deepak Jose <deepakjosp@gmail.com>
2024-01-25 12:37:46 +0530

    Updated estlintrc location

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,1 @@
-const defaultConfig = require("@bigbinary/neeto-commons-frontend/configs/eslint/index.js");
-
-const { mergeDeepLeft } = require("ramda");
-
-module.exports = mergeDeepLeft(
-  {
-    rules: {
-      "@bigbinary/neeto/no-missing-localization": "off",
-      "@bigbinary/neeto/webpack-aliases-and-jsconfig-paths-should-be-in-sync":
-        "off",
-    },
-  },
-  defaultConfig
-);
+module.exports = require("@bigbinary/neeto-commons-frontend/configs/nanos/eslint/index.js");
